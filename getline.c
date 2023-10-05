d5849e86e6bdd6cc190d53220b799385cf978883
Omar-algassim <omarggg2@gmail.com>
2023-10-05 15:31:27 -0700

    test

--- getline.c
+++ getline.c
@@ -10,15 +10,16 @@ int main()
 	{	
 		printf("$ ");
 		i = getline(&str, &len, stdin);
+		
 		if (i == -1)
 			exit(EXIT_FAILURE);
 
-		token[index] = strtok(str, " ");
+		*token = strtok(str, " ");
 
 	while (token[index] != NULL)
 	{
-		printf("%s\n", token[index]);
-		token[index] = strtok(NULL, " ");
+		printf("%s\n", &token);
+		token = strtok(NULL, " ");
 		index++;
 
 	}
