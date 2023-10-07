f927e11483c7a7bd794015a604dd998b5b00a706
Omar-algassim <omarggg2@gmail.com>
2023-10-07 16:26:39 -0700

    update

--- environ.c
+++ environ.c
@@ -1,23 +1,6 @@
 #include <stdio.h>
 #include "main.h"
 
-<<<<<<< HEAD
-int main(int ac, char **av,  char **env)
-{
-	size_t i = 0;
-	char *token, *tok, *var = env[i];
-	
-	while (var)
-	{
-	var = env[i];
-	//token = strtok(var, " ");
-	//tok = strtok(NULL, " ");
-	printf("%s\n", var);
-	i++;
-	}
-	free(var);
-	return (0);
-=======
 extern char **environ;
 
 char **get_env()
@@ -25,5 +8,4 @@ char **get_env()
 	char **envir = environ;
 	
 	return(envir);
->>>>>>> 03ea7e750ac975a558fc1a24bed01513a2178e68
 }
