d88a356da81a9427dd6148f00594e40569b403dd
Omar-algassim <omarggg2@gmail.com>
2023-10-14 08:14:03 -0700

    token and exe update

--- exe.c
+++ exe.c
@@ -1,13 +1,15 @@
 #include "main.h"
 
-int execute(char **args)
+int execute(char *args)
 {
+	char *const ar[] = {"-l", NULL};
 	char *allpaths = NULL;
 	char **paths = NULL;
+	char *arg = NULL;
 	char **path_and_file = NULL;
 	char *real_cmd = NULL;
 	int i = 0;
-	int size = 0;
+	int size = 1;
 	int check;
 	int status;
 	pid_t pid, ppid;
@@ -15,33 +17,46 @@ int execute(char **args)
 	allpaths = getenv("PATH");
 	paths = token_it(allpaths, ":\n\t");
 
-	while(paths[size])
+	while (paths[size])
 	{
+		//printf("%s\n", paths[size]);
 		size++;
 	}
-	//alocate mimory for the array
-	path_and_file = (char **)malloc(sizeof(char *) * (size + 1));
+	
+	//alocate memory for the array
+	path_and_file = (char **)malloc(sizeof(char *) * (size + 4));
 	if (path_and_file == NULL)
 		return (-1);
 
 	i = 0;
-	while (paths[i] != NULL && i < size)
-	{
+	//char **path_and_file = paths;
+	//while (path_and_file[i])
+	//{
 		//alocate memory for each pointer in the array
-		path_and_file[i] = (char *)malloc(strlen(paths[i]) + strlen(args[0]) + 2);
-		if(path_and_file[i] == NULL)
-			return (-1);
+	//	path_and_file[i] = (char *)malloc(strlen(paths[i]) + strlen(args[0]) + 2);
+		//if(path_and_file[i] == NULL)
+		//	return (-1);
 		//stick each path to the input comand
-		strcpy(path_and_file[i], paths[i]);
-		strcat(path_and_file[i], "/");
-		strcat(path_and_file[i], args[0]);
+		path_and_file[0] = paths[size - 1];
+		strcat(path_and_file[0], "/");
+		strcat(path_and_file[0], args);
+		//path_and_file[3] = '\0';
+		//printf("the path file is: %s\n", path_and_file[0]);
+
 		i++;
-	}
+	//}
+//	free(path_and_file);
+//	free(paths);
 	i = 0;
-	while(path_and_file[i] != NULL && i < size)
-	{
-		//check for the access and existence
-		check = access(path_and_file[i], X_OK);
+	
+	real_cmd = path_and_file[0];
+	
+	while(path_and_file[i])
+	//printf("the path_and_file: %s\n", path_and_file[0]);
+
+		
+	//*check for the access and existence
+		check = access(path_and_file[0], X_OK);
 		if(check == -1)		//if it's not the file I want free its mimmory
 		{
 			free(path_and_file[i]);
@@ -50,11 +65,12 @@ int execute(char **args)
 		}
 		if(check == 0)
 		{
-			real_cmd = path_and_file[i];
+			real_cmd = path_and_file[0];
 			break;
 		}
 		i++;
-	}
+	
+	//printf("the real command: %s\n", real_cmd);
 	//free the momiry of the array
 
 	free(path_and_file);
@@ -67,10 +83,10 @@ int execute(char **args)
 	}
 	if(pid == 0)
 	{
-		check = execve(real_cmd, args, NULL);
+		check = execve(real_cmd, ar, NULL);
 		if(check == -1)
 		{
-			printf("%s: command not found\n", args[0]);
+			printf("%s: command not found\n", args);
 			exit(EXIT_FAILURE);
 		}
 		if(check == 0)
@@ -83,5 +99,7 @@ int execute(char **args)
 		wait(&status);
 	}
 	free(real_cmd);
+	free(paths);
 	return (0);
 }
+
--- token1.c
+++ token1.c
@@ -1,42 +1,67 @@
 #include "main.h"
 
+/**
+  *token_it - function that ttokinaze the string depending on delimeter
+  *@ptr: the string will tokinze
+  *@del: the delimeter of tokinaze
+  *Return: the tokinzed array of pointer
+  */
+
 char** token_it(char* ptr, char* del)
 {
-    char** tokens = NULL;
-    char* copy = strdup(ptr);
+	char** tokens = NULL;
+	char* copy = strdup(ptr);
+	char* token;
+	int i;
 
-    if (copy == NULL)
-        return NULL;
+	if (copy == NULL)
+		return NULL;
 
-    tokens = (char**)malloc(2 * sizeof(char*)); // Allocate space for at least two pointers
+	tokens = (char**)malloc(2 * sizeof(char*)); // Allocate space for at least two pointers
 
-    if (tokens == NULL) {
-        free(copy);
-        return NULL;
-    }
-
-    char* token = strtok(copy, del);
-    int i = 0;
-
-    while (token) 
-    {
-        tokens[i] = strdup(token);
-
-        if (tokens[i] == NULL)
+	if (tokens == NULL) 
 	{
-            free(copy);
-            for (int j = 0; j < i; j++)
-                free(tokens[j]);
-            free(tokens);
-            return NULL;
-        }
+		free(copy);
+		return (NULL);
+	}
 
-        token = strtok(NULL, del);
-        i++;
-         }
+	token = strtok(copy, del);
+	i = 0;
 
-    tokens[i] = '\0'; // Add a null terminator to mark the end of the tokens array
+	while (token)
+	{
+		tokens[i] = malloc(sizeof(char*));
+		tokens[i] = strdup(token);
 
-    free(copy);
-    return tokens;
+		if (tokens[i] == NULL)
+		{
+			free(copy);
+			for (int j = 0; j < i; j++)
+				free(tokens[j]);
+			free(tokens);
+			return NULL;
+		}
+
+		token = strtok(NULL, del);
+		i++;
+	}
+
+	tokens[i] = NULL; // Set the last element of the tokens array to NULL
+
+	free(copy);
+	return (tokens);
 }
+/*int main()
+{
+	int i = 0;
+	char **tok;
+	char *ptr = "omar:algassim:hello:world:is:fun";
+
+	tok = token_it(ptr, ":\n\t");
+	
+	while (tok[i])
+	{
+		printf("%s\n", tok[i]);
+		i++;
+	}
+}*/
