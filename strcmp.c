a7b891932291743cd9d0fd907faa77d64276e808
Omar-algassim <omarggg2@gmail.com>
2023-10-18 12:20:09 -0700

    strcmp

--- strcmp.c
+++ strcmp.c
@@ -10,18 +10,13 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i, j;
-
-	i = 0;
-	j = 0;
-	
 	while (*s1 == *s2)
 	{
 		if (*s1 == '\0')
 			return (0);
 
-		*s1++;
-		*s2++;
+		s1++;
+		s2++;
 	}
 	return (*s1 - *s2);
 }
